304212517594976dd5425043352c7f8253f1c377
Amrutha A <160649644+Amruthaa14@users.noreply.github.com>
2024-09-07 18:17:23 +0530

    Create creating table.sql

---
+++ Creating Tables/creating table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Department VARCHAR(50),
+    Salary DECIMAL(10, 2),
+    HireDate DATE
+);
+
+CREATE TABLE Departments (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(50)
+);
